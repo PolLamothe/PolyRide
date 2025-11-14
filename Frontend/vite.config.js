e6dadb28343d50cbc486f6aff114183f9379c9b1
Pol Lamothe <lamothepol@gmail.com>
2025-11-14 21:58:32 +0100

    Frontend - .env PORT

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,16 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
     plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+    },
     optimizeDeps: {
         include: ['@radix-ui/themes'],
     },
+  }
 })
