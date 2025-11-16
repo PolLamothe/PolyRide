6a01a2b178246bbbff2d569395a60c96ce5ca3dc
86JLN <pitrejulien05@gmail.com>
2025-11-16 21:44:50 +0100

    Bouton modifier informations
    Ajout d'un bouton modifier (+ sauvegarder, annuler), et ajout d'informations à afficher dans la page account

--- Frontend/src/pages/Account.jsx
+++ Frontend/src/pages/Account.jsx
@@ -3,11 +3,14 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import polyrideDAO from "../dao/PolyrideDAO";
 import Header from "../components/Header.jsx";
+import './Account.css'
 
 function Account() {
+
+    const [isEditing, setIsEditing] = useState(false);
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
-
+    const [formData, setFormData] = useState(null);
     useEffect(() => {
         polyrideDAO.getProfile()
             .then(data => {
@@ -21,7 +24,20 @@ function Account() {
         document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         setUser(null);
     }
+    const editing = () =>{
+        setFormData(user);
+        setIsEditing(true);
+    }
 
+    const cancelEditing = () =>{
+        setFormData(null);
+        setIsEditing(false);
+    }
+
+    const saveInformations = () =>{
+        setUser(formData);
+        setIsEditing(false);
+    }
     const login = () => {
         navigate("/auth/login");
     }
@@ -31,19 +47,59 @@ function Account() {
     }
 
 
+
     return (
         <>
             <Header />
             <div className="account">
-                {user ? (
+                {/* affichage sans modif */}
+                {user  && !isEditing && (
                     <div>
                         <h2>Informations du compte</h2>
                         <p><strong>Nom d'utilisateur:</strong> {user.userName}</p>
                         <p><strong>Email:</strong> {user.email}</p>
                         <p><strong>Usage:</strong> {user.usage}</p>
+                        <p><strong>Emploi du temps:</strong> {user.calendarLink}</p>
+                        <p><strong>Numéro de téléphone:</strong> {user.phoneNumber}</p>
+                        <button onClick={() => editing()}>Modifier les informations</button>
                         <button onClick={() => handleLogout()}>Déconnexion</button>
                     </div>
-                ) : (
+                )
+                }
+                {/* affichage en modifiant */}
+                {user  && isEditing && (
+                    <div className="">
+                        <h2 className="test">Informations du compte</h2>
+                        <p><strong>Nom d'utilisateur:</strong> <input type="text" value={formData.userName} 
+                        onChange={(e)=> setFormData({...formData, userName: e.target.value})}/> </p>
+                        <p><strong>Email:</strong><input type="email" value={formData.email} 
+                        onChange={(e)=> setFormData({...formData, email: e.target.value})}/></p>
+                        <p></p>
+                        <p><strong>Emploi du temps :</strong> <input type="text" value={formData.calendarLink} 
+                        onChange={(e)=> setFormData({...formData, calendarLink: e.target.value})}/> </p>
+                        <p><strong>Numéro de téléphone:</strong> <input type="text" value={formData.phoneNumber} 
+                        onChange={(e)=> setFormData({...formData, phoneNumber: e.target.value})}/> </p>
+                        <p><strong>Usage:</strong>
+                        
+                        <input type="radio" name="usage" id="usage1" value={"Conducteur"}
+                        checked={formData.usage === "Conducteur"}
+                        onChange={(e)=> setFormData({...formData, usage: e.target.value})}/>
+                        <label htmlFor="usage1">Conducteur</label>
+
+                        <input type="radio" name="usage" value={"Passager"} 
+                        checked={formData.usage === "Passager"}
+                        onChange={(e)=> setFormData({...formData, usage: e.target.value})}/>
+                        <label htmlFor="usage1">Passager</label> 
+                        </p>
+                        
+                        <button onClick={() => cancelEditing()}>Annuler</button>
+                        <button onClick={() => saveInformations()}>Enregistrer</button>
+                        <button onClick={() => handleLogout()}>Déconnexion</button>
+                    </div>
+                )
+                }
+                {/* affichage si non connecté */}
+                {!user && (
                     <>
                         <button onClick={() => login()}>Se Connecter</button>
                         <button onClick={() => register()}>S'inscrire</button>
