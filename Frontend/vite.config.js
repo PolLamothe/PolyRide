3c12984481597b22b7f6a8e7b30f74d8f752b3ec
Pol Lamothe <lamothepol@gmail.com>
2025-11-16 22:36:37 +0100

    update vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,24 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const logRequests = () => ({
+  name: 'log-requests',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      // 👇 2. Imprime la méthode et l'URL de chaque requête
+      console.log(`[REQ]: ${req.method} ${req.url}`);
+      next(); // Passe la requête à Vite
+    });
+  },
+});
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      logRequests()
+    ],
     server: {
       port: parseInt(env.VITE_PORT) || 3000,
       host: '127.0.0.1',
