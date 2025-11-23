be84a1648eacfada3027ee101af7396d2dd92308
Pol Lamothe <lamothepol@gmail.com>
2025-11-23 15:51:23 +0100

    Frontend - usage switch tracking

--- Frontend/src/pages/Tracking.jsx
+++ Frontend/src/pages/Tracking.jsx
@@ -1,13 +1,63 @@
 import polyrideDAO from "../dao/PolyrideDAO";
 import Header from "../components/Header.jsx";
+import { useState, useEffect } from "react";
+import "./Tracking.css";
+
 
 function Tracking(){
+    const [user, setUser] = useState(null);
+    const [usage,setUsage] = useState(null)
+
+    useEffect(() => {
+        polyrideDAO.getProfile()
+            .then(data => {
+                console.log(data);
+                setUser(data);
+                if(data.usage == "Passager"){
+                    setUsage("Passager")
+                }else if(data.usage == "Conducteur et Passager" || data.usage == "Conducteur"){
+                    setUsage("Conducteur")
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    function updateUsage(newUsage){
+        if(newUsage != usage){
+            setUsage(newUsage)
+        }
+    }
+
     return <div>
         <Header />
-        <div>
-            <h2>Page de suivi des demandes de trajets</h2>
-            <p>Page de suivi des reqûetes des trajets</p>
-        </div>
+        {user && usage && (
+            <div>
+                <h2>Page de suivi des demandes de trajets</h2>
+                <p>Page de suivi des reqûetes des trajets</p>
+                <p>Pense à mettre ton compte en usage "Passager et Conducteur" (jte laisse stylisé tout ça)</p>
+                {user.usage == "Conducteur et Passager" && (
+                    <div>
+                        <button id={usage == "Conducteur" ? "activeUsageButton" : ""} onClick={()=>{updateUsage("Conducteur")}}>Conducteur</button>
+                        <button id={usage == "Passager" ? "activeUsageButton" : ""} onClick={()=>{updateUsage("Passager")}}>Passager</button>
+                    </div>
+                )}
+                {usage == "Conducteur" && (
+                    <div>
+                        <p>Affichages des trajets que des passagé on demandé</p>    
+                    </div>
+                )}
+                {usage == "Passager" && (
+                    <div>
+                        <p>Affichage des trajets que j'ai demandé aux conducteurs</p>
+                    </div>
+                )}
+            </div>
+        )}
+        {!user && (
+            <p>Chargement</p>
+        )}
     </div>
 }
 
